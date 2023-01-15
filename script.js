d2e553a971cdcaa270f5360a4095336a66e2d3a3
johnstonpc <102391762+johnstonpc@users.noreply.github.com>
2023-01-15 17:47:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function calcular (){
+    document.querySelector("main").style.display = "block";
+
+    var valorM = document.getElementById('MulherNumero')
+    var valorH = document.getElementById('HememNumero')
+    var valorH1 = valorH.value
+    var valorM1 = valorM.value
+
+    var valorMcm =  parseInt(valorM1);
+    var valorHcm =  parseInt(valorH1);
+
+
+    document.querySelector("#foto1").style.height=valorHcm+246+"px";
+    document.querySelector("#foto2").style.height=valorMcm+250+"px";
+
+}
