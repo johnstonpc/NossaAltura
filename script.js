2d8e32ddbd67a7f5980fd3aca0116460fbbf4dda
johnstonpc <102391762+johnstonpc@users.noreply.github.com>
2023-01-15 22:23:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function calcular (){
     var valorHcm =  parseInt(valorH1);
 
 
-    document.querySelector("#foto1").style.height=valorHcm+246+"px";
+    document.querySelector("#foto1").style.height=valorHcm+315+"px";
     document.querySelector("#foto2").style.height=valorMcm+250+"px";
 
 }
